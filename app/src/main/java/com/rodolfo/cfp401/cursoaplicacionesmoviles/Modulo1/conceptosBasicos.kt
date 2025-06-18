bc71aa4d9c0dd4ec1d4e2e589f56f6cc793274ec
Rodolfo <rodoceci@gmail.com>
2025-06-18 16:37:14 -0300

    Avance con ejercicios del modulo 1

--- app/src/main/java/com/rodolfo/cfp401/cursoaplicacionesmoviles/Modulo1/conceptosBasicos.kt
+++ app/src/main/java/com/rodolfo/cfp401/cursoaplicacionesmoviles/Modulo1/conceptosBasicos.kt
@@ -68,15 +68,35 @@ fun ejercicio71(){
 fun subtract(firstNumber: Int, secondNumber: Int): Int  {
     return firstNumber - secondNumber
 }
-fun ejercicio8(){
+fun ejercicio80(){
     val operatingSystem = "Chrome OS"
     val emailId = "sample@gmail.com"
 
     println(displayAlertMessage(operatingSystem, emailId))
 
 }
-fun displayAlertMessage(operatingSystem: String, emailId: String): String{
+fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String{
     return  "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+}
+fun ejercicio8(){
+    val firstUserEmailId = "user_one@gmail.com"
+    val firstUserOperatingSystem = "Linux"
+    // The following line of code assumes that you named your parameter as emailId.
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId, System = firtsUserOperatingSystem))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
 }
 
 fun main() {
