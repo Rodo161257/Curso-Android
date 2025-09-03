f40d38084aa80df4aecc09cd833d1c308503ede9
Rodolfo <rodoceci@gmail.com>
2025-09-03 14:13:11 -0300

    fix: error de llaves en CuentaBancaria

--- app/src/main/java/com/rodolfo/cfp401/cursoaplicacionesmoviles/POO/Persona.kt
+++ app/src/main/java/com/rodolfo/cfp401/cursoaplicacionesmoviles/POO/Persona.kt
@@ -2,8 +2,8 @@ package com.rodolfo.cfp401.cursoaplicacionesmoviles.POO
 
 import com.rodolfo.cfp401.cursoaplicacionesmoviles.Ejercicios.nombreDelDia
 
-abstract class Persona(val nombre: String)
-
-  fun presentarse(){
-    println("Hola soy ")
+abstract class Persona(val nombre: String){
+  fun presentarse() {
+      println("Hola soy ${nombre}")
+  }
 }
