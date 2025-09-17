59bff01d80af4a1307ee605857ac75da9fa0e71c
Rodolfo <rodoceci@gmail.com>
2025-09-17 16:03:40 -0300

    Avance con el problema del tostado de cafe y otros ejercicios

--- app/src/main/java/com/rodolfo/cfp401/cursoaplicacionesmoviles/POO/EjercicioClases/Guitarra.kt
+++ app/src/main/java/com/rodolfo/cfp401/cursoaplicacionesmoviles/POO/EjercicioClases/Guitarra.kt
@@ -2,6 +2,6 @@ package com.rodolfo.cfp401.cursoaplicacionesmoviles.POO.EjercicioClases
 
 class Guitarra: Instrumentos() {
     override fun hacerSonido() = println("do, re, mi, fa, sol, la, si")
-        TODO("Not yet implemented")
+
 
 }
